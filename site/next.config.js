bb2fd60a3cd51dd54cf4b7c375dd41585259c208
AlasdairWHClark <91890411+AlasdairWHClark@users.noreply.github.com>
2023-08-31 10:12:27 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'GTUKCDC',
+    description: 'Tailored Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/AlasdairWHClark/kasm-registry/',
+    contactUrl: 'https://alasdairwhclark.github.io//kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
